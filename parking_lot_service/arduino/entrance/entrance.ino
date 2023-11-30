d0a843f07aaee53b2212f12c57a424c1f81f16c7
allll1005 <150258023+allll1005@users.noreply.github.com>
2023-11-30 22:12:24 +0900

    Update entrance.ino

--- parking_lot_service/arduino/entrance/entrance.ino
+++ parking_lot_service/arduino/entrance/entrance.ino
@@ -1,8 +1,12 @@
-// 포트 정해두기
-
+// 포트 정해두기 //echo 13, trig 12
+#define echo 13 
+#define trig 12
 void setup() 
 {
   //핀 입출력 설정
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
 }
 
 void loop() 
@@ -13,6 +17,9 @@ void loop()
   //차량 허가(아직 안해도됨)
 
   //초음파 센서 작동
+  Serial.print(read_ultrasonic());
+  Serial.println("[cm]");
+  delay(500);
 
   //모터 90도
 
@@ -20,4 +27,18 @@ void loop()
 
   //현재 시간을 저장하고 모터 원위치
 
+}
+
+// 초음파 센서 함수(거리 계산)
+double read_ultrasonic(){
+  double return_time,howlong;
+  digitalWrite(trig, HIGH);
+  delay(5);
+  digitalWrite(trig, LOW);
+  return_time = pulseIn(echo, HIGH);
+  howlong = 340. * return_time / (2.*10000);
+  if (howlong > 99){
+  howlong = 99;
+  }
+  return howlong;
 }
