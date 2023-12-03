0244a332ef2557385ea51cdd0d7948678d2cf634
chmise <mnnnd4@gmail.com>
2023-12-04 03:02:09 +0900

    refactor : 코드 리펙토링

--- parking_lot_service/arduino/inOut/inOut.ino
+++ parking_lot_service/arduino/inOut/inOut.ino
@@ -1,59 +1,64 @@
-#include<Servo.h> 
-#define echo 12 
-#define trig 13
+#include <Servo.h>
+#define ECHO_PIN 12
+#define TRIG_PIN 13
 
-Servo servo;  //Servo 클래스로 servo객체 생성
-int servoAngle = 0;  // 각도를 조절할 변수 servoAngle
+Servo servo; //Servo 클래스로 servo객체 생성
+int servoAngle = 0; //Servo 각도를 조절할 변수 servoAngle
 unsigned long timer;
 long duration, distance;
-boolean is_reached = false; // "Reached"를 이미 출력했는지를 확인하는 변수
+boolean is_reached = false; //"Reached"를 이미 출력했는지를 확인하는 변수
 
 void setup() 
-{ //핀 입출력 설정
+{ 
+  //핀 입출력 설정
   Serial.begin(9600);
-  pinMode(trig,OUTPUT);
-  pinMode(echo,INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   servo.attach(A5);
 }
+//현재 시간 가져오기
 
-void loop() 
+
+long measureDistance() //초음파 센서 동작으로 거리가 리턴값임
 {
-  //현재 시간 가져오기
-  unsigned long currentMillis = millis();
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  return (duration / 2) / 29.1;
+}
 
-  //차량 허가(나중에)
-  
-  //초음파 센서 시리얼통신
-  digitalWrite(trig, LOW);  
-  delayMicroseconds(2); 
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10); 
-  digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
-  
-  if (distance < 10 && !is_reached) {  // 거리가 10 미만일 때
-    Serial.println("Reached");
-    is_reached = true; // "Reached"를 출력했음을 표시
-  } else if (distance >= 10 && is_reached) { // 거리가 다시 10 이상이 되었을 때
-    is_reached = false; // "Reached" 상태를 해제
-  }
-
-  
-  //서보모터 90도
-  if(distance < 10){
+void controlServo() //Servo 각도 조작 로직
+{
+  if (distance < 10) 
+  {
     servoAngle = 0;
     servo.write(servoAngle);
     timer = millis();
     is_reached = true;
-  }
-
-  //서보모터 원상태
-  if(millis() - timer > 5000 && distance >= 20){
+  } else if (millis() - timer > 5000 && distance >= 20) 
+  {
     servoAngle = 90;
     servo.write(servoAngle);
     is_reached = false;
   }
+}
 
+void loop() //메인루프
+{
+  distance = measureDistance();
+
+  if (distance < 10 && !is_reached) // 시리얼통신 조건
+  {
+    Serial.println("Reached");
+    is_reached = true;
+  } else if (distance >= 10 && is_reached) 
+  {
+    is_reached = false;
+  }
+
+  controlServo();
   delay(500);
 }
